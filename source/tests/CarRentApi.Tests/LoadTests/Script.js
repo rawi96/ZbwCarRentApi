0b7325a82a45e3be584bfe2abff961ee27e7464a
leafar396 <raphi.wirth@gmail.com>
2021-09-06 19:56:42 +0200

    add loadtest html export

--- source/tests/CarRentApi.Tests/LoadTests/Script.js
+++ source/tests/CarRentApi.Tests/LoadTests/Script.js
@@ -1,5 +1,7 @@
 ﻿import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 export let options = {
     stages: [
@@ -14,4 +16,11 @@ export default function () {
     let res = http.get('https://httpbin.org/');
     check(res, { 'status was 200': (r) => r.status == 200 });
     sleep(1);
+}
+
+export function handleSummary(data) {
+    return {
+        "testResults/load-tests.html": htmlReport(data),
+        stdout: textSummary(data, { indent: " ", enableColors: true }),
+    };
 }
